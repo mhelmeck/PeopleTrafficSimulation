e49c97043dd9b57b29d63bd71e223fae8e4a4af6
Maciej Hełmecki <mhelmecki@gmail.com>
2018-11-24 23:54:38 +0100

    Save drawed board to file

--- BoardBuilder.py
+++ BoardBuilder.py
@@ -1,32 +1,47 @@
 import pygame
+import pickle
 
 # Properties
 max_width = 400
 max_height = 400
-step = 3
+min_width = 1
+min_height = 1
+step = 5
 
-builder_size = 20
+builder_size = 10
 (builder_x, builder_y) = (0, 0)
 
 is_finished = False
+is_building = False
 blue_color = (0, 128, 255)
+red_color = (231, 76, 60)
+
+points = []
 
 # Methods
-def set_new_position_when_pressed(x_pos, y_pos):
+def set_new_position(x_pos, y_pos):
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_UP]:
         if y_pos >= step:
             y_pos -= step
     if pressed[pygame.K_DOWN]:
-        if y_pos <= max_height - builder_size:
+        if y_pos <= max_height - builder_size - step:
             y_pos += step
     if pressed[pygame.K_LEFT]:
         if x_pos >= step:
             x_pos -= step
     if pressed[pygame.K_RIGHT]:
-        if x_pos <= max_width - builder_size:
+        if x_pos <= max_width - builder_size - step:
             x_pos += step
     return x_pos, y_pos
+
+
+def build():
+    for x in range(builder_size):
+        for y in range(builder_size):
+            new_point = (builder_x + x, builder_y + y)
+            if new_point not in points:
+                points.append(new_point)
 
 
 # Init and main loop
@@ -40,16 +55,34 @@ while not is_finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_finished = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            is_building = not is_building
 
     # Move
-    (builder_x, builder_y) = set_new_position_when_pressed(builder_x, builder_y)
+    (builder_x, builder_y) = set_new_position(builder_x, builder_y)
+    if is_building:
+        build()
 
     # Draw
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, blue_color, pygame.Rect(builder_x,
-                                                     builder_y,
-                                                     builder_size,
-                                                     builder_size))
+    color = red_color if is_building else blue_color
+    pygame.draw.rect(screen, color, pygame.Rect(builder_x,
+                                                builder_y,
+                                                builder_size,
+                                                builder_size))
+    for point in points:
+        pygame.draw.rect(screen, color, pygame.Rect(point[0],
+                                                    point[1],
+                                                    min_width,
+                                                    min_height))
     # Render
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(30)
+
+# Save data to file
+with open("points.txt", "wb") as fp:
+    pickle.dump(points, fp)
+
+# Exit
+pygame.quit()
+exit(0)
